8fc5c7c62390b36b51f54214c1a849543e214087
CaoKao <bksaechao@gmail.com>
2019-11-01 05:05:17 -0700

    created a firebase database for the trainscheduler and linked it to the javascript file

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,23 @@
-$("#btn").on("click", function (event) {
-    event.preventDefault();
+$(document).ready(function () {
+    var firebaseConfig = {
+        apiKey: "AIzaSyC9JxsGCnXOPvhfmqZhxQqSQUd4id1Iv6o",
+        authDomain: "train-scheduler-76f0c.firebaseapp.com",
+        databaseURL: "https://train-scheduler-76f0c.firebaseio.com",
+        projectId: "train-scheduler-76f0c",
+        storageBucket: "train-scheduler-76f0c.appspot.com",
+        messagingSenderId: "622609698920",
+        appId: "1:622609698920:web:88ac8554bb28404d52ebdd"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+    var database = firebase.database();
+
+    $("#button").on("click", function (event) {
+        event.preventDefault();
+        $(".trainData").val('');
+
+        var trainVal = $
+        var newTrain = $("<tr></tr>")
+    });
+
 })
